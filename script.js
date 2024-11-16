0650cf48bc26ea676330e6bfa6e67f69b3072416
eliswi <e.swiatkowska@lelewel.poznan.pl>
2024-11-16 19:17:05 +0100

    Update script.js - klasy stylizujace, odpowiedzi tylko dla poprzednich dni

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ var odpowiedziListaStr = "";
 var dataDzisiejsza = new Date();
 
 // TODO: zamien finalnie na grudniowe daty !!!
-for (var listopad = new Date(2024, 10, 1); listopad <= dataDzisiejsza; listopad.setDate(listopad.getDate() + 1)) {
-    var dzienListopada = (listopad.getDate() < 10 ? "0" : "") + listopad.getDate();
+for (var danyDzien = new Date(2024, 10, 1); danyDzien <= dataDzisiejsza; danyDzien.setDate(danyDzien.getDate() + 1)) {
+    var dzienListopadaStr = (danyDzien.getDate() < 10 ? "0" : "") + danyDzien.getDate();
   
     zadaniaListaStr += 
       '<li><a href="/kalendarz-grudniowy-2024/zadania_' 
-      + dzienListopada 
-      + '_listopada_2024.pdf" class="" target="_blank">Zadania ' 
-      + dzienListopada 
+      + dzienListopadaStr 
+      + '_listopada_2024.pdf" class="link-primary list-group-item list-group-item-action" target="_blank">Zadania ' 
+      + dzienListopadaStr 
       + ' listopada 2024</a></li>';
-  
+
+    // Nie pokazuj odpowiedzi w danym dniu (odpowiedzi na zadania z danego dnia pojawiają się w dniu następującym).
+    if (danyDzien === dataDzisiejsza) {
+        break;
+    }
+    
     odpowiedziListaStr += 
       '<li><a href="/kalendarz-grudniowy-2024/odpowiedzi_' 
-      + dzienListopada
-      + '_listopada_2024.pdf" class="" target="_blank">Odpowiedzi ' 
-      + dzienListopada
+      + dzienListopadaStr
+      + '_listopada_2024.pdf" class="link-primary list-group-item list-group-item-action" target="_blank">Odpowiedzi ' 
+      + dzienListopadaStr
       + ' listopada 2024</a></li>';
 }
 
